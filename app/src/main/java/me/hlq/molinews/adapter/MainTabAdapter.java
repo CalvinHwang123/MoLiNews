3448cf5c467cdaeb93ce78774a20ed82eb1c0ba6
CalvinHwang123 <CalvinHwang123@gmail.com>
2016-09-25 12:30:07 +0800

    Add Something

---
+++ app/src/main/java/me/hlq/molinews/adapter/MainTabAdapter.java
@@ -0,0 +1,44 @@
+package me.hlq.molinews.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+import me.hlq.molinews.fragment.TabFragment;
+
+/**
+ * 顶部标题选项的适配器
+ * Created by HLQ on 2016/08/31.
+ */
+public class MainTabAdapter extends FragmentPagerAdapter{
+
+    private List<TabFragment> lists;
+    private String[] titles;
+
+    //注意这里FragmentPagerAdapter类的构造函数只有一个FragmentManager参数，要先调用super的manager再自定义
+    //构造方法才不会报错
+    public MainTabAdapter(FragmentManager manager, List<TabFragment> lists,String[] titles){
+
+        super(manager);
+        this.lists = lists;
+        this.titles = titles;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return lists.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return titles.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
+    }
+
+}
